6ef0f6eac2b6de88da3e3cdb8e899457391b2fcb
Ethan Muller <hi@emu.media>
2020-11-23 12:19:52 -0500

    remove "latest"

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,27 +4,45 @@ import SEO from "../components/seo"
 import MeImg from "../images/crusty-hard-128.gif"
 
 const homeMainStyle = {
-  maxWidth: '32em',
+  maxWidth: "32em",
 }
 
 const IndexPage = () => (
   <>
     <SEO title="emu media" />
-    
+
     <header>
       <img src={MeImg} alt="crusty drawing of me" />
       <div>
-        <h1>hi, i am <a href="https://www.ethanmuller.com/">ethan muller</a>.</h1>
+        <h1>
+          hi, i am <a href="https://www.ethanmuller.com/">ethan muller</a>.
+        </h1>
         <p>here are some pieces of media i've made&nbsp;for&nbsp;you:</p>
       </div>
     </header>
-    
+
     <main style={homeMainStyle}>
       <ul>
-        <li><strong>Latest:</strong> <a href="https://omg.emu.media/">oh my gosh</a> is a music track with a companion web app. The app lets you change the rate of audio playback.</li>
-        <li><a href="https://cadavre-exquis.emu.media/public/">cadavre exquis</a> is a web app in which we draw a very tall picture together. drawings show up live. it's fun to play in groups. :)</li>
-        <li><a href="https://goofball.fun">goofball</a> is a mobile game in which you touch and drag to hit the long-eyed dudes. don't touch the ground to get a hi combo.</li>
-        <li><a href="https://emuuuu.itch.io/wooltergeist">wooltergeist</a> is a game in which you are a demon! possess and unpossess to progress! playable in your browser! made in 48 hours.</li>
+        <li>
+          <a href="https://omg.emu.media/">oh my gosh</a> is a music track with
+          a companion web app. The app lets you change the rate of audio
+          playback.
+        </li>
+        <li>
+          <a href="https://cadavre-exquis.emu.media/public/">cadavre exquis</a>{" "}
+          is a web app in which we draw a very tall picture together. drawings
+          show up live. it's fun to play in groups. :)
+        </li>
+        <li>
+          <a href="https://goofball.fun">goofball</a> is a mobile game in which
+          you touch and drag to hit the long-eyed dudes. don't touch the ground
+          to get a hi combo.
+        </li>
+        <li>
+          <a href="https://emuuuu.itch.io/wooltergeist">wooltergeist</a> is a
+          game in which you are a demon! possess and unpossess to progress!
+          playable in your browser! made in 48 hours.
+        </li>
       </ul>
     </main>
   </>
