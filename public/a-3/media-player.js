419ffcce6e9b38751e39bc31b6f2685342dff919
Ethan Muller <hi@emu.media>
2021-03-20 17:24:24 -0700

    lil fixes

--- public/a-3/media-player.js
+++ public/a-3/media-player.js
@@ -53,7 +53,7 @@ function setup() {
 
 
   playbtn = createButton(
-    '<svg width="28" height="33" viewBox="0 0 28 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28 16.5L0.249998 32.5215L0.25 0.47853L28 16.5Z" fill="#fff"/></svg>'
+    '<svg width="28" height="33" viewBox="0 0 28 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28 16.5L0.249998 32.5215L0.25 0.47853L28 16.5Z" fill="currentColor"/></svg>'
   );
   playbtn.elt.id = "play";
   playbtn.elt.classList.add("throb");
