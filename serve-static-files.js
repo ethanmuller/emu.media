cd70ac65501f06e0875e89aec3a97f0a2519f5f1
Ethan Muller <ethanbmuller@gmail.com>
2022-08-03 11:39:08 -0400

    wip

--- serve-static-files.js
+++ serve-static-files.js
@@ -5,6 +5,6 @@ const path = require('path')
 
 app.use('/', express.static(path.join(__dirname, "public")))
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
